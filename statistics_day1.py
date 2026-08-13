1f1db2c9ee01078812ae06b60bbc8adb0ff8651e
TanishaSaini28 <sainitanisha091@gmail.com>
2026-08-13 21:16:30 +0530

    functions performed

---
+++ statistics_day1.py
@@ -0,0 +1,50 @@
+import numpy as np 
+data = np.array([10,20,30,40,50])
+
+mean = np.mean(data)
+print(mean)
+
+median = np.median(data)
+print(median)
+
+varience = np.var(data)
+print(varience)
+
+standard_deviation = np.std(data)
+print(standard_deviation)
+
+minimum = np.min(data)
+print(minimum)
+
+maximum = np.max(data)
+print(maximum)
+
+sum = np.sum(data)
+print(sum)
+
+import numpy as np 
+data = np.array([10,20,30,40,50])
+
+mean = np.mean(data)
+print(mean)
+
+median = np.median(data)
+print(median)
+
+varience = np.var(data)
+print(varience)
+
+standard_deviation = np.std(data)
+print(standard_deviation)
+
+minimum = np.min(data)
+print(minimum)
+
+maximum = np.max(data)
+print(maximum)
+
+sum = np.sum(data)
+print(sum)
+
+length = len(data)
+print(length)

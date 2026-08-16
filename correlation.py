b0652f4b8e1c917a3c43f844bdc52575e158d577
TanishaSaini28 <sainitanisha091@gmail.com>
2026-08-16 12:56:16 +0530

    Update correlation.py

--- correlation.py
+++ correlation.py
@@ -1,5 +1,5 @@
 import numpy as np 
 data1 = np.array([10, 20, 30, 40, 50])
 data2 = np.array([15, 25, 35, 45, 55])
-correlation = np.corrcoef(data1 , data2)
+correlation = np.corrcoef(data1 , data2)[0,1]
 print(correlation)
